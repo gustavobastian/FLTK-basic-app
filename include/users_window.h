574d17719b11e4668356d1d5d083c43240bdd45a
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-02-09 12:51:04 -0300

    adding some comments for the doxygen info

--- include/users_window.h
+++ include/users_window.h
@@ -65,26 +65,114 @@ class users_Window:public Fl_Group
         bool *authorization;
     public:
         users_Window(int x, int y);
+        /**
+         * @brief Construct a new users Window::users Window object
+         * 
+         * @param w 
+         * @param h 
+         * @param data 
+         * @param localDB 
+         * @param state 
+         */
+
         users_Window(int w, int h, int data, databaseService *localDB, bool *state);
 
         unsigned int get_value();
+        /**
+         * @brief set local value for the user window
+         * 
+         * @param valueP 
+         * @return int 
+         */
         int set_value(unsigned int valueP);
-        static void search_cb(Fl_Widget* w,void* data);      
-        static void create_cb(Fl_Widget* w,void* data);      
-        static void update_cb(Fl_Widget* w,void* data);      
+        /**
+         * @brief callback for looking for a user
+         * 
+         * @param w pointer to button
+         * @param data pointer to parent window
+         */
+        static void search_cb(Fl_Widget* w,void* data);   
+        /**
+         * @brief callback function for the create button
+         * 
+         * @param w pointer to the button
+         * @param data pointer to the parent window
+         */
+   
+        static void create_cb(Fl_Widget* w,void* data);
+        /**
+         * @brief callback function for the update button
+         * 
+         * @param w pointer to the button
+         * @param data pointer to the window
+         */      
+        static void update_cb(Fl_Widget* w,void* data); 
+                
+        /**
+         * @brief callback function for the quit button, erases the current person and hide send button
+         * 
+         * @param w 
+         * @param data 
+         */     
         static void quit_cb(Fl_Widget* w,void* data);      
         static void send_cb(Fl_Widget* w,void* data);      
         
 
+        /**
+         * @brief Checks if a user is logged in the system and enables functionalities
+         * 
+         * @param _state 
+         */
         bool *get_authorization(){ return this->authorization; }  
+        /**
+         * @brief binds the object state to external state
+         * 
+         * @param _state 
+         */
         void set_authorization(bool *_state);
 
-        static void update_pass_cb(Fl_Widget* w,void* data);        
-        static void update_firstName_cb(Fl_Widget* w,void* data);        
-        static void update_lastName_cb(Fl_Widget* w,void* data);        
-        static void update_mode_cb(Fl_Widget* w,void* data);        
-        static void update_username_cb(Fl_Widget* w,void* data);        
 
+        /**
+         * @brief callback that update the password object when the widget is modified
+         * 
+         * @param w pointer to the input
+         * @param data pointer to the window
+         */
+        static void update_pass_cb(Fl_Widget* w,void* data);        
+
+        /**
+         * @brief function that update the first name when the widget is modified
+         * 
+         * @param w pointer to the widget
+         * @param data pointer to the window
+         */
+        static void update_firstName_cb(Fl_Widget* w,void* data);        
+        /**
+         * @brief function that update the last name when the widget is modified
+         * 
+         * @param w pointer to the widget
+         * @param data pointer to the window
+         */
+        static void update_lastName_cb(Fl_Widget* w,void* data);
+        /**
+         * @brief function that update the mode when the widget is modified
+         * 
+         * @param w pointer to the widget modified 
+         * @param data pointer to the window
+         */
+        static void update_mode_cb(Fl_Widget* w,void* data);        
+
+        /**
+         * @brief function that update the username when the widget is modified
+         * 
+         * @param w pointer to the widget
+         * @param data pointer to the window
+         */
+        static void update_username_cb(Fl_Widget* w,void* data);        
+        /**
+         * @brief Destroy the users Window::users Window object
+         * 
+         */
         ~users_Window();
 
 };
